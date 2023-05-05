32fd5c54500e01298c6aed2d0816395c07c0c718
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-04 19:51:17 -0700

    reviewing models and starting read me

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -15,13 +15,18 @@ ProductTag.init(
       product_id: {
         type: DataTypes.STRING,
         allowNull: false,
+        references: {
+          model: "product",
+          key: "id",
+          // unique: false
+        }
       },
       tag_id: {
         type: DataTypes.INTEGER,
         references: {
-          model: "Tag",
+          model: "tag",
           key: "id",
-          unique: false
+          // unique: false
         }
       }
     },
